163d7636621af07d51533627c5e6b856349b7378
Amir B. Kahna <amir.kahna2@gmail.com>
2026-04-28 15:54:09 +0100

    Adding library with react

---
+++ src/components/CardsList.jsx
@@ -0,0 +1,38 @@
+import { Button, Card } from "react-bootstrap";
+
+function CardsList({ books }) {
+  return (
+    <>
+      {books.map((i) => (
+        <Card
+          key={i.asin}
+          style={{
+            width: "300px",
+            margin: "10px",
+            backgroundColor: "transparent",
+            color: "white",
+            border: "1px solid white",
+          }}
+        >
+          <Card.Img
+            variant="top"
+            src={i.img}
+            style={{ height: "300px", objectFit: "cover" }}
+          />
+
+          <Card.Body className="d-flex flex-column">
+            <Card.Title>{i.title}</Card.Title>
+            <Card.Text>{i.category}</Card.Text>
+            <Card.Text>{i.price} €</Card.Text>
+
+            <Button className="mt-auto" variant="success">
+              Buy
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+export default CardsList;
